654bac06f5b5ecd7aee5601ccba20af8c02a081e
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-05-19 22:04:53 +0800

    refactor: Update LandingButton position and size in Landing section

--- src/components/LandingButton.tsx
+++ src/components/LandingButton.tsx
@@ -6,7 +6,7 @@ export default function LandingButton() {
   const isBelowMd = useMediaQuery({ query: "(max-width: 767px)" });
 
   return (
-    <div className="absolute -bottom-[17rem] z-50 flex w-4/5 items-center justify-center md:bottom-[2rem]">
+    <div className="absolute -bottom-[calc(17rem+3rem)] z-50 flex w-4/5 items-center justify-center md:bottom-[2rem]">
       <div className="relative">
         <div className="absolute -left-16 -top-6 z-[80] size-16 sm:-left-24 sm:-top-10 sm:size-24">
           <img src="/assets/arrow-icon.png" alt="Arrow Icon" />
